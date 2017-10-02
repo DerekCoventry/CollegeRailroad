86099756169ca565b634d9020d9de24b9eee1066
Derek Coventry <derek@Dereks-MacBook-Pro.local>
2017-10-02 09:55:43 -0400

    added login activity and add article

--- CollegeRailroad/app/src/main/java/com/example/derek/collegerailroad/MainActivity.java
+++ CollegeRailroad/app/src/main/java/com/example/derek/collegerailroad/MainActivity.java
@@ -2,6 +2,15 @@ package com.example.derek.collegerailroad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.annotation.SuppressLint;
+import android.app.ActionBar.LayoutParams;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button yourButton = (Button) findViewById(R.id.log_but);
+
+        yourButton.setOnClickListener(new OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
+
 }
