314e12bc8993cfa53621d077492c57ce032879e6
Adrien Lindner <lindner.54@osu.edu>
2017-10-04 03:08:33 -0400

    Map now gets current location.

--- CollegeRailroad/app/src/main/java/com/example/derek/collegerailroad/MapsActivity.java
+++ CollegeRailroad/app/src/main/java/com/example/derek/collegerailroad/MapsActivity.java
@@ -1,5 +1,9 @@
 package com.example.derek.collegerailroad;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -14,9 +18,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    protected LocationManager mLocationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -27,20 +34,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     /**
      * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  },
+                    11 );
+        }
+        Location coordinates = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         // Add a marker in Sydney and move the camera
-        LatLng columbus = new LatLng(40, -83);
-        mMap.addMarker(new MarkerOptions().position(columbus).title("Marker in Columbus"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(columbus));
+        LatLng location = new LatLng(coordinates.getLatitude(), coordinates.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(location).title("Current Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
 }
