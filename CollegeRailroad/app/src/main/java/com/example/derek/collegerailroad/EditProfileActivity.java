aeb9cbe3608cf62ba4123a65538574198b8b870b
Adrien Lindner <lindner.54@osu.edu>
2017-10-30 20:47:20 -0400

    Non-database things for viewing and changing profiles done

--- CollegeRailroad/app/src/main/java/com/example/derek/collegerailroad/EditProfileActivity.java
+++ CollegeRailroad/app/src/main/java/com/example/derek/collegerailroad/EditProfileActivity.java
@@ -1,8 +1,12 @@
 package com.example.derek.collegerailroad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -10,6 +14,13 @@ public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        Button updateProfile = (Button) findViewById(R.id.update_profile_but);
+        updateProfile.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                // If profile was updated
+                Toast.makeText(EditProfileActivity.this, "Profile updated!", Toast.LENGTH_SHORT).show();
+            }
+        });
         String username = "username123";
         String email = "test123@test.com";
         String name = "My Name";
@@ -19,5 +30,19 @@ public class EditProfileActivity extends AppCompatActivity {
         editUsername.setText(username);
         editEmail.setText(email);
         editName.setText(name);
+        Button changePassword = (Button) findViewById(R.id.change_password_but);
+        changePassword.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                EditText currentPassword = (EditText) findViewById(R.id.current_password);
+                EditText newPassword = (EditText) findViewById(R.id.new_password);
+                EditText confirmPassword = (EditText) findViewById(R.id.confirm_password);
+                // If password was changed
+                Toast.makeText(EditProfileActivity.this, "Password Changed!", Toast.LENGTH_SHORT).show();
+                currentPassword.setText("");
+                newPassword.setText("");
+                confirmPassword.setText("");
+
+            }
+        });
     }
 }
