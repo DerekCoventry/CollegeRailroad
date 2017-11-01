91ef5d132af6fb5b8a2fc6b3c5976158cce9726a
Adrien Lindner <lindner.54@osu.edu>
2017-11-01 19:38:33 -0400

    Signing up now returns back to the homepage and logging out works

--- CollegeRailroad/app/src/main/java/com/example/derek/collegerailroad/BaseAppCompatActivity.java
+++ CollegeRailroad/app/src/main/java/com/example/derek/collegerailroad/BaseAppCompatActivity.java
@@ -3,8 +3,12 @@ package com.example.derek.collegerailroad;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 public abstract class BaseAppCompatActivity extends AppCompatActivity {
@@ -42,6 +46,34 @@ public abstract class BaseAppCompatActivity extends AppCompatActivity {
         // If logout preferences exist
         //Sign out GET to collegerailsroad.com/user/logout
         //Set preferences to null
-        Toast.makeText(this, "Signed out", Toast.LENGTH_SHORT).show();
+        SharedPreferences userInfo = getSharedPreferences("userInfo", MODE_PRIVATE);
+        final String logoutUrl = "http://www.collegerailroad.com/user/logout";
+        WebView webView = new WebView(this);
+        webView.setVisibility(View.GONE);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                startActivity(new Intent(BaseAppCompatActivity.this, HomeActivity.class));
+                return false;
+            }
+        });
+        //you can load an html code
+        webView.loadData("yourCode Html to load on the webView " , "text/html" , "utf-8");
+        // you can load an URL
+        webView.loadUrl(logoutUrl);
+        if(userInfo.contains("USER_ID")) {
+            SharedPreferences.Editor userInfoEditor = userInfo.edit();
+            userInfoEditor.putString("USER_ID", null);
+            userInfoEditor.putString("USER_NAME", null);
+            userInfoEditor.putString("CSRF", null);
+            userInfoEditor.putString("LOGOUT", null);
+            userInfoEditor.putString("RAW", null);
+            userInfoEditor.putString("BASIC_AUTH", null);
+            userInfoEditor.commit();
+            Toast.makeText(this, "Signed out", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, HomeActivity.class));
+        }else{
+            Toast.makeText(this, "Already signed out", Toast.LENGTH_SHORT).show();
+        }
     }
 }
