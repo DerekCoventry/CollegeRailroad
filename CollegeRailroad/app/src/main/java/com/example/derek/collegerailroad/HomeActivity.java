272549f91b268e80164bdb04754ca459de611581
Adrien Lindner <lindner.54@osu.edu>
2017-11-01 14:06:34 -0400

    Removed login and sign up buttons from home page when user is logged in

--- CollegeRailroad/app/src/main/java/com/example/derek/collegerailroad/HomeActivity.java
+++ CollegeRailroad/app/src/main/java/com/example/derek/collegerailroad/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.derek.collegerailroad;
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -12,6 +13,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -41,19 +43,25 @@ public class HomeActivity extends BaseAppCompatActivity implements SearchFragmen
         TextView intro = (TextView) findViewById(R.id.intro);
         intro.setText("Welcome to College Railroad! This app allows students to buy/sell books with each other without the bookstore middleman markup. Current apps/websites exist for the sole purpose of making money. Businesses and bookstores make excessive amounts of money off of students, and for a very long time students have accepted this as a way of life. Additionally, Facebook pages, and craigslist posts try to accomplish our goal, but the market it waiting for an app to satisfy the need. Having an app be created by potential users allows for an alternative view on the textbook market.");
         Button loginButton = (Button) findViewById(R.id.log_but);
-        loginButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
-            }
-        });
-
         Button signupButton = (Button) findViewById(R.id.signup_but);
-
-        signupButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                startActivity(new Intent(HomeActivity.this, web.class));
-            }
-        });
+        SharedPreferences userInfo = getSharedPreferences("userInfo", MODE_PRIVATE);
+        if(userInfo.contains("USER_ID")){
+            ViewGroup loginLayout = (ViewGroup) loginButton.getParent();
+            ViewGroup signupLayout = (ViewGroup) signupButton.getParent();
+            loginLayout.removeView(loginButton);
+            signupLayout.removeView(signupButton);
+        }else {
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                }
+            });
+            signupButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    startActivity(new Intent(HomeActivity.this, web.class));
+                }
+            });
+        }
         Button listButton = (Button) findViewById(R.id.list_but);
 
         listButton.setOnClickListener(new View.OnClickListener(){
