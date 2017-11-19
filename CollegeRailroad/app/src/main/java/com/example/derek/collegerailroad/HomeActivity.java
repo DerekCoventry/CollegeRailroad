6c33ed4b26449fb850e9388d652b76876fe09450
Adrien Lindner <lindner.54@osu.edu>
2017-11-19 01:12:57 -0500

    Search option stays same on rotation on home activity

--- CollegeRailroad/app/src/main/java/com/example/derek/collegerailroad/HomeActivity.java
+++ CollegeRailroad/app/src/main/java/com/example/derek/collegerailroad/HomeActivity.java
@@ -1,49 +1,32 @@
 package com.example.derek.collegerailroad;
 
-import android.*;
-import android.annotation.SuppressLint;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.w3c.dom.Text;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class HomeActivity extends BaseAppCompatActivity implements SearchFragment.OnFragmentInteractionListener{
-    private TextView mTextView;
+    private static String OPTION = "option";
+    private String starting_opt;
     private String search_opt;
-    public String[] options = new String[]{ "ISBN", "Title", "Author"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(savedInstanceState != null) {
+            starting_opt = savedInstanceState.getString(OPTION, "ISBN");
+        }
     }
 
     @Override
@@ -67,9 +50,13 @@ public class HomeActivity extends BaseAppCompatActivity implements SearchFragmen
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                search_opt = "ISBN";
+                search_opt = starting_opt;
             }
         });
+        if(starting_opt != null) {
+            int position = adapter.getPosition(starting_opt);
+            opt.setSelection(position);
+        }
         TextView intro = (TextView) findViewById(R.id.intro);
         intro.setText("Welcome to College Railroad! This app allows students to buy/sell books with each other without the bookstore middleman markup. Current apps/websites exist for the sole purpose of making money. Businesses and bookstores make excessive amounts of money off of students, and for a very long time students have accepted this as a way of life. Additionally, Facebook pages, and craigslist posts try to accomplish our goal, but the market it waiting for an app to satisfy the need. Having an app be created by potential users allows for an alternative view on the textbook market.");
         Button loginButton = (Button) findViewById(R.id.log_but);
@@ -111,6 +98,13 @@ public class HomeActivity extends BaseAppCompatActivity implements SearchFragmen
         menu.removeItem(R.id.action_home);
         return true;
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString(OPTION, search_opt);
+    }
+
     @Override
     public void onFragmentInteraction(Uri uri){
     }
